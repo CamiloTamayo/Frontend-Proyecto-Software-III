1019c1904011e35a7fff5ddf8d632243c29c3cc3
CamiloTamayo <juanc.tamayoa@uqvirtual.edu.co>
2022-05-17 22:02:21 -0500

    V 0.0.2 Creación componentes
    Se crea la carpeta "Componentes" y se crea cada uno de los componentes necesarios para este proyecto.

---
+++ Frontend/src/app/Componentes/control-ingreso/control-ingreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-ingreso',
+  templateUrl: './control-ingreso.component.html',
+  styleUrls: ['./control-ingreso.component.css']
+})
+export class ControlIngresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ControlIngresoComponent } from './Componentes/control-ingreso/control-ingreso.component';
+import { CrearCarreraComponent } from './Componentes/crear-carrera/crear-carrera.component';
+import { GenerarQrComponent } from './Componentes/generar-qr/generar-qr.component';
+import { IngresoComponent } from './Componentes/ingreso/ingreso.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { LugarComponent } from './Componentes/lugar/lugar.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { RegistroUsuarioComponent } from './Componentes/registro-usuario/registro-usuario.component';
+import { RolComponent } from './Componentes/rol/rol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlIngresoComponent,
+    CrearCarreraComponent,
+    GenerarQrComponent,
+    IngresoComponent,
+    InicioComponent,
+    LoginComponent,
+    LugarComponent,
+    NavbarComponent,
+    RegistroUsuarioComponent,
+    RolComponent
   ],
   imports: [
     BrowserModule,
